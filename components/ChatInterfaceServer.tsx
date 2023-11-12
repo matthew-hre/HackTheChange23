ffa01f4ae9af2cc9ff6ae5c2e5da215bd010715d
matthew-hre <whycardboard@gmail.com>
2023-11-11 22:37:09 -0700

    last push of the night

--- components/ChatInterfaceServer.tsx
+++ components/ChatInterfaceServer.tsx
@@ -12,6 +12,8 @@ const tryChat = async (message: string) => {
   console.log("tryChat Started");
   const calendarData = await getEventsFromCalendar();
 
+  console.log("calendarData: " + JSON.stringify(calendarData));
+
   const assistant = await openAIClient.beta.assistants.create({
     name: "CalPal",
     description: "An assistant to help you manage your calendar",
@@ -21,7 +23,7 @@ const tryChat = async (message: string) => {
 
   const thread = await openAIClient.beta.threads.create();
 
-  //console.log("thread.id: " + thread.id);
+  console.log("thread.id: " + thread.id);
 
   const messageToSend = await openAIClient.beta.threads.messages.create(
     thread.id,
@@ -34,7 +36,7 @@ const tryChat = async (message: string) => {
   const run = await openAIClient.beta.threads.runs.create(thread.id, {
     assistant_id: assistant.id,
     instructions:
-      "You are CalPal, a friendly university calendar assistant bot. Your job is to help people organize their schedules. Follow these steps when receiving a message:\n\n1. Users will message you with a question or a concern, as well as their calendar data. When a user messages you, find the event they are referring to from their calendar data.\n\n2. If the user is asking about information about an existing event, communicate that information.\n\n3. Else, if a user is expressing a concern about an event, or needs help creating one, add an event to their calendar to help. Events could be anything from creating study sessions, scheduling meetings, or booking out personal time.\n\n4. Make a single event that would resolve their problem. Choose a date and time for the event that would work with their existing schedule. Ensure that no schedule conflicts exist when creating events.",
+      "You are CalPal, a friendly university calendar assistant bot. Your job is to help people organize their schedules. Follow these steps when receiving a message:\n\n1. Users will message you with a question or a concern, as well as their calendar data. When a user messages you, find the event they are referring to from their calendar data.\n\n2. If the user is asking about information about an existing event, communicate that information.\n\n3. Else, if a user is expressing a concern about an event, or needs help creating one, add an event to their calendar to help. Events could be anything from creating study sessions, scheduling meetings, or booking out personal time.\n\n4. Make one single event that would resolve their problem. Choose a date and time for the event that would work with their existing schedule. Ensure that no schedule conflicts exist when creating events.",
   });
 
   let runStatus = await openAIClient.beta.threads.runs.retrieve(
@@ -51,7 +53,7 @@ const tryChat = async (message: string) => {
       run.id
     );
 
-    //console.log("runStatus.status: " + runStatus.status);
+    console.log("runStatus.status: " + runStatus.status);
 
     if (runStatus.status === "failed") {
       return "Sorry, I couldn't understand that. Please try again.";
@@ -65,7 +67,7 @@ const tryChat = async (message: string) => {
   const runResults = await openAIClient.beta.threads.messages.list(thread.id);
 
   // @ts-ignore
-  //console.log(runResults.data[0].content[0].text.value);
+  console.log(runResults.data[0].content[0].text.value);
 };
 
 const ChatInterfaceServer = () => {
