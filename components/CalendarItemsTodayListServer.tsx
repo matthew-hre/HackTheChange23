9e690ab101736c8b9a9dcc6d455b122ad8c69de2
Dylan Borchert <dylanbor01@gmail.com>
2023-11-12 11:04:24 -0700

    hosting fix

--- components/CalendarItemsTodayListServer.tsx
+++ components/CalendarItemsTodayListServer.tsx
@@ -5,7 +5,7 @@ import getEventsFromTodayCalendar from "@/hooks/getEventsFromTodayCalendar";
 const CalendarItemsListServer = async () => {
   const events = await getEventsFromTodayCalendar();
 
-  return events.length > 0 ? <CalendarItemsListClient events={events} /> :
+  return events.length > 0 ? <CalendarItemsListClient events={events} showdate={false} /> :
     <div className="text-center font-normal py-3">
       No Events Today
     </div>;
