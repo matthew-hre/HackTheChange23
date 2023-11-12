2ec4772ecc374c130a328868e6b1c1315fa44714
Dylan Borchert <dylanbor01@gmail.com>
2023-11-11 21:06:25 -0700

    styles and fixed time bug

--- components/CalendarItemsListClient.tsx
+++ components/CalendarItemsListClient.tsx
@@ -1,32 +1,37 @@
+import { debug } from "console";
+
 const CalendarItemsListClient = ({ events }: { events: any[] }) => {
 
   const formatDate = (date: string) => {
     const dateObj = new Date(date);
-    let hours = dateObj.getHours();
+    let hours = dateObj.getHours() + 7;
     let minutes = dateObj.getMinutes();
 
-    //convert 0 to 12
-    let tag = (hours < 12) ? "AM" : "PM";
+    //add leading zero
+    let minutesString = minutes < 10 ? '0' + minutes as string : minutes as unknown as string;
+
+    let tag = hours >= 12 ? 'PM' : 'AM';
 
     //convert to 12 hour format
-    hours = dateObj.getHours() > 12 ? dateObj.getHours() - 12 : dateObj.getHours();
+    hours = hours > 12 ? hours - 12 : hours;
 
-
-
-
-    return `${hours}:${minutes} ${tag}`;
+    return `${hours}:${minutesString} ${tag}`;
   }
 
-  console.log(events);
   return (
     <div className="custom-shadow rounded-md my-2 p-2 ">
       {events.map((event) => (
+        console.log(event),
         <div key={event.id} className="border-b-2 border-gray-200 last:border-b-0 flex py-3 justify-between">
-          <div className="bg-primary w-16 h-16 rounded-lg">
+          <div className={`w-14 h-14 rounded-lg p-1`} style={{ backgroundColor: "#" + event.colour }}>
+
 
           </div>
-          <p className="font-normal debug-s flex-grow p-2">{event.summary}</p>
-          <p className="font-normal">{formatDate(event.start_datetime)}</p>
+          <p className="font-normal flex-grow p-2">{event.summary}</p>
+          <div className="h-14 flex justify-around flex-col">
+            <p className="font-normal h-5 rounded-full py-0.5 px-2 text-xs text-white text-center" style={{ backgroundColor: "#" + event.colour }}>{formatDate(event.start_datetime)}</p>
+            <p className="font-normal h-5 rounded-full py-0.5 px-2 text-xs text-white text-center" style={{ backgroundColor: "#" + event.colour }}>{formatDate(event.end_datetime)}</p>
+          </div>
         </div>
       ))}
     </div>
